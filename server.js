35e41dbc75855dcd7c73987c309d73c10ca2e5da
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-06-28 05:28:10 +0000

    automatically backed up by learn

--- server.js
+++ server.js
@@ -3,13 +3,21 @@
 const http         = require('http');
 const finalhandler = require('finalhandler');
 const Router       = require('router');
+const bodyParser = require('body-parser');
 
 const router = new Router();
+router.use(bodyParser.json())
+let messages = []
 
 router.get('/', (request, response) => {
   // A good place to start!
-  response.end();
+  response.setHeader('Content-Type', 'text/plain; charset=utf-8')
+  response.end('Hello, World!');
 });
+
+router.post('./message', (request,response) => {
+  console.log(request.body)
+})
 
 const server = http.createServer((request, response) => {
   router(request, response, finalhandler(request, response));
